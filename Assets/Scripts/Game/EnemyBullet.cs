681e97ca21945b471909218453a9a8615d5598b3
Tan Wei Xin <0115190@kdu-online.com>
2019-02-26 23:22:08 +0800

    WX_Prefabs' changes
    Changed some stuff for Normie, LaserShooter, Chess Room, Enemy Bullet

--- Assets/Scripts/Game/EnemyBullet.cs
+++ Assets/Scripts/Game/EnemyBullet.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class EnemyBullet : Projectile
 {
+    public float damage;
+
     public override void Start()
     {
-
+        //Used to override Projectile's Start function
     }
 
     public void SetShootDirection(Vector3 dir)
     {
         shootDir = dir;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerCoreController>().controller.GetDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
