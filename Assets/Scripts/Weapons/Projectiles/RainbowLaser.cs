d41be91c205351ea10c9724e50cb0eef25044ccc
KaiKheng <khengl97@gmail.com>
2019-03-13 13:45:26 +0800

    KK_Update
    update

--- Assets/Scripts/Weapons/Projectiles/RainbowLaser.cs
+++ Assets/Scripts/Weapons/Projectiles/RainbowLaser.cs
@@ -6,25 +6,29 @@ public class RainbowLaser : Projectile
 {
     #region Rainbow Laser Variables
     [Header("Rainbow Laser")]
+    public LayerMask destroyableLayer;
+
     public LineRenderer lineRenderer;
 
     #endregion
 
     public override void Start()
     {
-        shootDir = InputManager.Instance.GetShootDir();
         lineRenderer = GetComponent<LineRenderer>();
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, shootDir);
+        shootDir = InputManager.Instance.GetShootDir();
 
-        lineRenderer.enabled = true;
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, transform.position + shootDir);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, shootDir,fireRange, destroyableLayer);
 
+        if(hit.collider != null)
+        {
+            Debug.Log(hit.collider);   
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, hit.collider.transform.position);
+        }
 
-
-
-        
+        Destroy(this);
         /*for (int i = 1; i < laserSegmentLength; i++)
         {
             var pos = new Vector3(i * 1.2f, Mathf.Sin(i + Time.time * Random.Range(0.5f, 1.3f)), 0f);
